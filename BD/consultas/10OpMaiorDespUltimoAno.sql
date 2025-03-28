4b9cdb1db5b7eeb2251a7981cbff8c362f77feae
Katiane <katiane.soares@fatec.sp.gov.br>
2025-03-27 23:27:28 -0300

    Queries desenvolvidas

---
+++ BD/consultas/10OpMaiorDespUltimoAno.sql
@@ -0,0 +1,13 @@
+USE ans_db;
+
+SELECT
+    o.Razao_Social,
+    SUM(d.VL_SALDO_FINAL) AS Total_Despesas
+FROM demonstracoes_contabeis d
+JOIN operadoras o ON d.Reg_ANS = o.Registro_ANS
+WHERE
+    d.Descricao_CONTA_CONTABIL = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR '
+    AND d.Data  BETWEEN '2024-01-01' AND '2024-12-31'
+GROUP BY o.Razao_Social
+ORDER BY Total_Despesas DESC
+LIMIT 10;
